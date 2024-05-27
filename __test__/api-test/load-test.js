bbd7282a5feb0618d23596458f7b34da238c4ea9
Priscila Caimi <priscila.caimi@hotmail.com>
2024-05-27 13:15:06 -0300

    adição de report html

--- __test__/api-test/load-test.js
+++ __test__/api-test/load-test.js
@@ -1,5 +1,16 @@
+/**
+ * https://github.com/benc-uk/k6-reporter/blob/main/tests/grpc.js
+ */
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 import http from 'k6/http';
 import { check, sleep } from 'k6';
+
+
+// const RAMP_TIME = __ENV.RAMP_TIME || '1s'
+// const RUN_TIME = __ENV.RUN_TIME || '2s'
+// const USER_COUNT = __ENV.USER_COUNT || 20
+// const SLEEP = __ENV.SLEEP || 0.5
 
 export let options = {
     stages: [
@@ -10,7 +21,14 @@ export let options = {
 };
 
 export default function () {
-    let res = http.get('https://jsonplaceholder.typicode.com/posts');
+    let res = http.get('https://test-api.k6.io/public/crocodiles/');
     check(res, { 'status was 200': (r) => r.status == 200 });
     sleep(1);
 }
+
+export function handleSummary(data) {
+    return {
+      'summary.html': htmlReport(data, { debug: false }),
+      stdout: textSummary(data, { indent: ' ', enableColors: true }),
+    }
+  }
